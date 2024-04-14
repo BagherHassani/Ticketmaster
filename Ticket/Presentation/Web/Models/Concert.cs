bcb0897ac47571e168368ae29d595f0853586639
Reza <mrdev1020@gmail.com>
2024-04-15 00:37:34 +0330

    feat: parameters in Blazor

---
+++ Ticket/Presentation/Web/Models/Concert.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Models;
+
+public class Concert
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- Ticket/Presentation/Web/Program.cs
+++ Ticket/Presentation/Web/Program.cs
@@ -7,6 +7,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//builder.Services.AddCors(corsOptions =>
+//{
+//    corsOptions.AddDefaultPolicy(corsPolicyBuilder =>
+//    {
+//        corsPolicyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+//    });
+//});
+
+//builder.Services.AddTransient<interface, class> ();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddOidcAuthentication(options =>
@@ -15,6 +25,7 @@ builder.Services.AddOidcAuthentication(options =>
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
+
 
 builder.Services.AddMudServices();
 
