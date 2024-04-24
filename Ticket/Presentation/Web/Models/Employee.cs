8a2da87d6793c33faa87067ed7fa40b9a0cbe2dd
Reza <mrdev1020@gmail.com>
2024-04-25 00:42:27 +0330

    feat: employee get & create

---
+++ Ticket/Presentation/Web/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Web.Models;
+
+
+
+public class SingleData
+{
+    [JsonPropertyName("data")]
+    public Employee Employee { get; set; }
+}
+
+
+
+public class Data
+{
+    [JsonPropertyName("data")]
+    public IEnumerable<Employee> Employees { get; set; }
+}
+
+
+public class Employee
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("employee_name")]
+    public string EmployeeName { get; set; }
+}
+
+
+
+public class Createemployee
+{
+    public string Name { get; set; }
+    public string Salary { get; set; }
+    public string Age { get; set; }
+}
